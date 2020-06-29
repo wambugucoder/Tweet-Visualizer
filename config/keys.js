4ed3d3e7968ad4cb4d027e6a3e3a64d73062e8b5
JosWambugu <josphatwambugu77@gmail.com>
2020-06-29 17:24:52 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,20 +1,20 @@
 module.exports = {
   //TWTTER BOT AND OAUTH
 
-  consumer_key:"KzlTHgdaI6cXvYFhUHrFUY2ra",
-  consumer_secret:"9rZMqSmWEog4Na9IPSKQcYlBGsm7OT6SkWC0dEPJHzTZidvreY",
-  access_token:"1205571465736790017-TAOCxoHP2ZsInpPoudpH0ZdQ7AFZYi",
-  access_token_secret:"18yrkYrsZlDiKRFWx6pM4hAhGihUwteqxFvVtL7CCMbDx",
+  consumer_key:"",
+  consumer_secret:"",
+  access_token:"",
+  access_token_secret:"",
  
   //MONGO KEY
 
-  mongoURI:"mongodb://wambugu:josphatwambugu12@cluster0-shard-00-00-vtvw5.mongodb.net:27017,cluster0-shard-00-01-vtvw5.mongodb.net:27017,cluster0-shard-00-02-vtvw5.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+  mongoURI:"",
 
   //COOKIE KEY
-  cookie:"peanut_butter",
+  cookie:"",
 
 
 //GOOGLE OAUTH KEYS
-clientID:"363273928837-ughvtmlm73k2431t2bu7ti9884uackl3.apps.googleusercontent.com",
-clientSecret:"q8bpNqQZXH7sDFpMAQQOyndr"
+clientID:"",
+clientSecret:""
 };
